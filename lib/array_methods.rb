a6f3df5b959624989386d278bbfae57a3843e547
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-29 20:56:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,10 +9,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  x = array[0]
+  maxval = array[0]
   array.length.times { |index|
   if array[index] > x
-    x = array[index]
+    maxval = array[index]
   end
   }
   x
